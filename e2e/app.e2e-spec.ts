5c6c1477fc226765db7a0882b2ed41d3b3eb3495
angular-cli <angular-cli@angular.io>
2016-11-11 09:53:34 -0500

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { CodelabPage } from './app.po';
+
+describe('codelab App', function() {
+  let page: CodelabPage;
+
+  beforeEach(() => {
+    page = new CodelabPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
