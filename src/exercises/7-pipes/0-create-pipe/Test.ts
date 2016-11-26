6f1ad4d2b51cb32fd0236c800ec9947332112002
Aziz <ayokubjonov@hudsonhs.com>
2016-11-26 01:08:16 -0500

    fuzzy time pipe & test

--- src/exercises/7-pipes/0-create-pipe/Test.ts
+++ src/exercises/7-pipes/0-create-pipe/Test.ts
@@ -1,30 +1,31 @@
 import {TestBed} from '@angular/core/testing';
-import {FuzzyPipe, evalJs} from './solution/FuzzyPipe'; // Solution prefix will be stripped-out by the app
+import {FuzzyTime, evalJs} from './FuzzyPipe'; // Solution prefix will be stripped-out by the app
 import "reflect-metadata";
 
 let metadata;
 beforeEach(() => {
   TestBed.resetTestingModule();
-  TestBed.configureTestingModule({declarations: [AppComponent]});
-  metadata = Reflect.getMetadata("annotations", AppComponent);
+  TestBed.configureTestingModule({declarations: [FuzzyTime]});
+  metadata = Reflect.getMetadata("annotations", FuzzyTime);
 });
 
 describe('Pipe', () => {
-  it('Create a class called AppComponent', () => {
-    chai.expect(typeof evalJs('AppComponent')).equals('function');
+  it('Create a pipe called FuzzyTime', () => {
+    chai.expect(typeof evalJs('FuzzyTime')).equals('function');
   });
-  it('Export the created class', () => {
-    chai.expect(typeof AppComponent).equals('function');
-  });
-  it('Add a Component decorator for the class', () => {
-    chai.expect(metadata).is.not.undefined
-  });
-  it('Add selector to the component annotation config', () => {
-    chai.expect(metadata[0].selector).equals('my-app');
+  it('Export it', () => {
+    chai.expect(typeof FuzzyTime).equals('function');
   });
 
-  it('Add a template which would output <h1>Hello world!</h1>', () => {
-    chai.expect(metadata[0].template).equals('<h1>Hello world!</h1>');
+  it('Make it work', () => {
+    let fuzzyTime = new FuzzyTime();
+    jasmine.clock().install();
+    jasmine.clock().mockDate(new Date('2016-11-26'));
+    chai.expect(fuzzyTime.transform('2016-11-24')).equals('2 days ago');
+    chai.expect(fuzzyTime.transform('2012-11-25', )).equals('4 years ago');
+    chai.expect(fuzzyTime.transform('2010-11-25')).equals('6 years ago');
+    jasmine.clock().uninstall();
   });
+
 });
 
