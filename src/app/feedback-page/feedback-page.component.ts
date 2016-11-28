15c0cb93c6c9d2f6d96d16e14c8477961a070f38
Aziz <ayokubjonov@hudsonhs.com>
2016-11-28 00:21:05 -0500

    feedbackers rank

--- src/app/feedback-page/feedback-page.component.ts
+++ src/app/feedback-page/feedback-page.component.ts
@@ -10,13 +10,27 @@ import {StateService} from "../state.service";
 export class FeedbackPageComponent implements OnInit {
   feedbackList:FirebaseListObservable<any[]>;
   constructor(public state: StateService, private angularFire: AngularFire) { }
-
+  listOfFeedbackers = [];
   ngOnInit() {
     //TODO: use auth property in the state to skip authentication proccess
     this.angularFire.auth.login().then(authState => {
       this.state.setAuth(authState.auth);
     });
     this.feedbackList = this.angularFire.database.list('/feedback');
+
+    this.feedbackList.subscribe(list => {
+      let countedFeedbackers = [];
+      list.forEach((val, index) => {
+        if(countedFeedbackers.filter(x => x === val.name).length === 0){
+          this.listOfFeedbackers.push(
+            {numberOfFeedback: list.filter(x => x.name == val.name && countedFeedbackers.filter(x => x == val.name).length == 0).length, name: val.name}
+          );
+        }
+        countedFeedbackers.push(val.name);
+      });
+      this.listOfFeedbackers.sort((a,b) => {return b.numberOfFeedback - a.numberOfFeedback});
+      console.log(this.listOfFeedbackers);
+    });
   }
 
   simulate(state){
