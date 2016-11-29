ec3c3b95f6a45f41905d1f347283f4293a96bcd5
Keith <keithio@users.noreply.github.com>
2016-11-28 18:48:01 -0800

    Provide link to the Angular 2 cheatsheet

--- src/exercises/1-bootstrap/2-bootstrap/Bootstrap.ts
+++ src/exercises/1-bootstrap/2-bootstrap/Bootstrap.ts
@@ -1,2 +1,4 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from '../1-module/solution/AppModule';
+
+// https://angular.io/docs/ts/latest/guide/cheatsheet.html
