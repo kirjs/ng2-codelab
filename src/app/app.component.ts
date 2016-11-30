9320fb6967dbd2a545e4dd4cbfecd48040206040
Aziz <ayokubjonov@hudsonhs.com>
2016-11-29 21:34:46 -0500

    track user progress + anonymous authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { StateService } from "./state.service";
+import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,41 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user_progresses = this.angularFire.database.list('/user_progress');
+  currentState = {};
+  auth = {uid:''};
+
+  constructor(private state: StateService, private angularFire: AngularFire) {
+    this.state.update.debounceTime(500).subscribe((state) => {
+      this.currentState = state;
+      if(this.auth.uid){
+        this.user_progresses.update(this.auth.uid, JSON.parse(JSON.stringify(state)));
+      }
+    });
+
+    this.angularFire.auth.subscribe((authState) => {
+      if (!authState) {
+        this.angularFire.auth.login({ provider: AuthProviders.Anonymous, method: AuthMethods.Anonymous }).then(authData => {
+          this.auth = authData;
+          this.user_progresses.update(authData.uid, {});
+        });
+      }
+      else {
+        this.auth = authState;
+        let progress = angularFire.database.object('/user_progress/' + this.auth.uid).remove();
+        progress.then((progress) => {
+          if (progress) {
+            this.state.simulateState(progress);
+          }
+        }).catch(() => {
+          console.log(`can't recover progress`);
+        });
+      }
+    });
+
+
+  }
+  ngOnInit() {
+
+  }
 }
