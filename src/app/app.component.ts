74e97945c94695476b585f158eb532c5a592c43e
Aziz <ayokubjonov@hudsonhs.com>
2016-12-18 16:09:20 -0500

    sending progress fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component} from "@angular/core";
-import {StateService} from "./state.service";
-import {AngularFire} from "angularfire2";
+import { Component } from "@angular/core";
+import { StateService } from "./state.service";
+import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from 'angularfire2';
+import {appConfig} from './app-config.service';
 
 @Component({
   selector: 'app-root',
@@ -8,52 +9,40 @@ import {AngularFire} from "angularfire2";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //user_progresses = this.angularFire.database.list('/user_progress');
-  currentState = {};
-  remoteStateLoaded = false;
-  auth = {uid: ''};
-
+  user_progresses;
+  auth;
   constructor(private state: StateService, private angularFire: AngularFire) {
-
-    /*
-     this.state.update.debounceTime(500).subscribe((state) => {
-     this.currentState = state;
-     if (this.auth.uid && this.remoteStateLoaded) {
-     console.log('setting state to firebase', this.auth.uid);
-     this.user_progresses.update('testtest', JSON.parse(JSON.stringify(state)));
-     }
-     });
-
-     let authObservable = this.angularFire.auth.subscribe((authState) => {
-     if (!authState) {
-     console.log('not authorized');
-     this.angularFire.auth.login({
-     provider: AuthProviders.Anonymous,
-     method: AuthMethods.Anonymous
-     }).then(authData => {
-     console.log(authData.uid);
-     this.auth = authData;
-     this.user_progresses.update('testtest', JSON.parse(JSON.stringify(this.currentState)));
-     this.remoteStateLoaded = true;
-     });
-     }
-     else {
-     console.log('authorized');
-     this.auth = authState;
-     console.log(this.auth.uid, "@id");
-     angularFire.database.object('/user_progress/' + this.auth.uid).subscribe((progress) => {
-     if (progress) {
-     this.remoteStateLoaded = true;
-     this.state.simulateState(progress);
-     }
-     });
-     }
-     //authObservable.unsubscribe();
-     });
-     */
   }
 
   ngOnInit() {
+    if(appConfig.feedbackEnabled){
+        this.user_progresses = this.angularFire.database.list('/user_progress');
+        this.auth = { uid: '' };
 
+        let authObservable = this.angularFire.auth.subscribe((authState) => {
+          if (!authState) {
+            console.log('not authorized');
+            this.angularFire.auth.login({
+              provider: AuthProviders.Anonymous,
+              method: AuthMethods.Anonymous
+            }).then(authData => {
+              console.log(authData.uid);
+              this.auth = authData;
+            }).catch(() => {console.log('Authorization failed. Try refreshing the page.')});
+          }
+          else {
+            console.log('authorized');
+            this.auth = authState;
+          }
+
+          this.state.update.debounceTime(500).subscribe((state) => {
+            if (this.auth.uid) {
+              console.log('setting state to firebase', this.auth.uid);
+              this.user_progresses.update(this.auth.uid, JSON.parse(JSON.stringify(state)));
+            }
+          });
+          authObservable.unsubscribe();
+        });
+    }
   }
 }
