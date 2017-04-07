a1cd35c36a1be73a50b6488bd581f4d400afc0ae
Kirill Cherhkashin <kirill@kcherkashin.org>
2017-04-07 14:09:03 -0600

    Add slides section

--- exercises/codelab-config-service.ts
+++ exercises/codelab-config-service.ts
@@ -3,6 +3,7 @@ import {Injectable} from '@angular/core';
 import {ng2tsConfig} from './ng2ts/ng2ts';
 import {testCodelabConfig} from './test-codelab/test-codelab';
 import {UxiConfig} from './uxi/uxi';
+import {SlidesConfig} from './slides/slides';
 
 declare const require;
 
@@ -10,11 +11,13 @@ declare const require;
 export class CodelabConfigService {
   public codelabs: [CodelabState];
 
+
   constructor() {
     this.codelabs = [
       Object.assign(ng2tsConfig, {selectedMilestoneIndex: 0}),
       Object.assign(UxiConfig, {selectedMilestoneIndex: 0}),
-      Object.assign(testCodelabConfig, {selectedMilestoneIndex: 0})
+      Object.assign(testCodelabConfig, {selectedMilestoneIndex: 0}),
+      Object.assign(SlidesConfig, {selectedMilestoneIndex: 0})
     ]
   }
 }
